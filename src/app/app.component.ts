b19d3fc841567b591c54401d6d4c09f8fb9995e2
cloudsony999 <cloudsony999@gmail.com>
2022-04-25 13:09:16 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'firsproject';
+  ename="Mercy";
+  address="Chennai";
+
+  display()
+  {
+    alert("Welcome to CG Angular Course!!!");
+  }
 }
